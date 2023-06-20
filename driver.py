6bbaac8a4ea2121aa19ccb56f4fd7a3084ba3102
JoshWager1 <josh.wager22@gmail.com>
2023-06-20 11:40:45 -0400

    created function to return the controller items

--- driver.py
+++ driver.py
@@ -9,3 +9,8 @@ class Laser_driver:
     def __init__(self, name, port_=9000):
         self._connection = connect(name, port=port_)
 
+
+    #returns dict_item list of the laser DLC pro controllers
+    def get_controller_items(self): #maybe rename
+        return self._connection.root.controllers.items()
+    
