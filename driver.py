0ab9d7b39f53ff456874794c3483cf67627e98cd
JoshWager1 <josh.wager22@gmail.com>
2023-06-20 12:10:56 -0400

    program will not only control a single DLC Pro

--- driver.py
+++ driver.py
@@ -1,37 +1,29 @@
 from rpyc import connect
-#import toptica_laserskd
 
-class Laser_driver:
+class Laser:
 
-    _connection = None
+    _controller = None
 
-    #establishes connection
-    def __init__(self, name, port_=9000):
-        self._connection = connect(name, port=port_)
+    # establishes connection
+    def __init__(self, name, controller, port_=9000):
+        self._controller = connect(name, port=port_).root.controllers[controller]
 
-
-    #returns dict_item list of the laser DLC pro controllers
-    def get_controller_items(self): #maybe rename
-        return self._connection.root.controllers.items()
-    
-# returns the emission status of the chose laser
-    def get_emission(self, controller, laser_number=None):
+    # returns the emission status of the chose laser
+    def get_emission(self, laser_number=None):
         if (laser_number==None or (laser_number in (1,2)) == False):
             laser = ''
         else:
             laser = f'laser{laser_number}.'
-        return self._connection.root.controllers[controller].get(f'{laser}emission')
+        return self._controller.get(f'{laser}emission')
 
 
     # returns the voltage offset of the chose laser
-    def get_voltage_offset(self, controller, laser_number):
+    def get_voltage_offset(self, laser_number):
         assert(laser_number in (1, 2))
-        return self._connection.root.controllers[controller].get(f'laser{laser_number}.scan.offset')
+        return self._controller.get(f'laser{laser_number}.scan.offset')
 
 
     # Sets a laser to a chosen laser offset
-    def set_voltage_offset(self, controller, laser_number, voltage_offset):
+    def set_voltage_offset(self, laser_number, voltage_offset):
         assert(laser_number in (1, 2))
-        return self._connection.root.controllers[controller].set(f'laser{laser_number}.scan.offset', voltage_offset)
-    
-
+        return self._controller.set(f'laser{laser_number}.scan.offset', voltage_offset)
