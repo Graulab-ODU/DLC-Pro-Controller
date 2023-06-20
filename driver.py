7dbc29bb10058dc41222b256e9689af154b94999
JoshWager1 <josh.wager22@gmail.com>
2023-06-20 11:41:29 -0400

    Added function to get emission status of a laser

--- driver.py
+++ driver.py
@@ -14,3 +14,14 @@ class Laser_driver:
     def get_controller_items(self): #maybe rename
         return self._connection.root.controllers.items()
     
+# returns the emission status of the chose laser
+    def get_emission(self, controller, laser_number=None):
+        if (laser_number==None or (laser_number in (1,2)) == False):
+            laser = ''
+        else:
+            laser = f'laser{laser_number}.'
+        return self._connection.root.controllers[controller].get(f'{laser}emission')
+
+
+  
+
