23c9a1a8363bbb744742c08f52b3fb6e3635070f
JoshWager1 <josh.wager22@gmail.com>
2023-06-28 15:20:20 -0400

    updated so program controls individual laser

--- DLC_Pro_Controller/driver.py
+++ DLC_Pro_Controller/driver.py
@@ -4,36 +4,37 @@ class Laser:
 
     _controller = None
     _controller_name = ''
+    _laser_number = 0
 
     # establishes connection
-    def __init__(self, name, controller, port_=9000):
+    def __init__(self, name, controller, laser_number, port_=9000):
+        
         try:
             self._network_Connection = connect(name, port=port_)
         except:
             print("Network connection Failure")
             return
-        self._controller = self._network_Connection.root.controllers[controller]
         self._controller_name = controller
+        
+        # ensures laser number is between 1 and 2
+        assert(laser_number in (1, 2))
+        self._laser_number = laser_number
+        self._controller = self._network_Connection.root.controllers[controller]
+        
 
     # returns the emission status of the chose laser
-    def get_emission(self, laser_number=None):
-        if (laser_number==None or (laser_number in (1,2)) == False):
-            laser = ''
-        else:
-            laser = f'laser{laser_number}.'
-        return self._controller.get(f'{laser}emission')
+    def get_emission(self):
+        return self._controller.get(f'{self._laser_number}emission')
 
 
     # returns the voltage offset of the chose laser
-    def get_voltage_offset(self, laser_number):
-        assert(laser_number in (1, 2))
-        return self._controller.get(f'laser{laser_number}.scan.offset')
+    def get_voltage_offset(self):
+        return self._controller.get(f'laser{self._laser_number}.scan.offset')
 
 
     # Sets a laser to a chosen laser offset
-    def set_voltage_offset(self, laser_number, voltage_offset):
-        assert(laser_number in (1, 2))
-        return self._controller.set(f'laser{laser_number}.scan.offset', voltage_offset)
+    def set_voltage_offset(self, voltage_offset):
+        return self._controller.set(f'laser{self._laser_number}.scan.offset', voltage_offset)
     
     def __repr__(self):
         return f'DLC_Pro_controller({self._controller_name})'
